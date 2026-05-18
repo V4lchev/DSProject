83f0e09579bcca0b7736b474e193e7b615e57c16
Veselin Valchev <v.valchev05@gmail.com>
2026-05-18 18:56:29 +0300

    Final commit

--- XMLParserProject/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"Elements"},{"l":"Files"},{"l":"Parser"},{"l":"Writer"},{"l":"XMLParser"}];updateSearchResults();
